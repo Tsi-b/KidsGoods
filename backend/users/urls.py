cc968e82fa9317715bc00941aff51caa0d2f66db
Dani-El <tsib.5828@gmail.com>
2025-10-21 11:24:38 +0300

    Updated backend: user registration, login, JWT auth, and cookie setup

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, CookieTokenObtainPairView, protected_view
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,7 +8,10 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),   # returns access and refresh
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('protected/', protected_view, name='protected'),
+    # path('token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
